de5f5c6f84e7ce2b1fcc3a950c3516ae0e70d8f5
Levkovich Polina <levkovich.polina@gmail.com>
2018-12-04 05:17:29 +0400

    Task01.MathOperations project was created

---
+++ src/Task01.MathOperations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01.MathOperations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double d1 = 7;
+			double d2 = 6;
+
+			Console.WriteLine("+ {0}", d1 + d2);
+			Console.WriteLine("- {0}", d1 - d2);
+			Console.WriteLine("* {0}", d1 * d2);
+			Console.WriteLine("/ {0}", d1 / d2);
+
+			int i1 = 9;
+			double d3 = 0.1 + 0.2;
+			double d4 = 0.3;
+			double epsilon = 0.000001;
+			Console.WriteLine(d3 - d4 < epsilon);
+		}
+	}
+}
