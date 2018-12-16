03d19ba24951dd33943ffb2b900cf880c672fcdc
Levkovich Polina <levkovich.polina@gmail.com>
2018-12-16 12:37:07 +0400

    изучила логические операции

---
+++ src/Task04.SimpleTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04.SimpleTypes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double d1;
+			float fl1;
+			string str1;
+			bool bool1 = false;
+			int i1;
+			byte byte1;
+			char char1;
+
+			bool bool3 = true && true && true && false;
+			bool bool4 = true || false || false; 
+
+
+			
+			bool bool2 = !bool1;
+		}
+	}
+}
