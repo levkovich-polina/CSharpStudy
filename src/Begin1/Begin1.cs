fa896925e15cbf69d30120dedf360fa4caacd5e4
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-06 19:45:50 +0300

    решена задача Begin1 из задачника

---
+++ src/Begin1/Begin1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin1
+{
+    class Begin1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = int.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число. Следует ввести положительное число");
+                return;
+            }
+            Console.WriteLine($"P={number*4}");
+            Console.ReadLine();
+        }
+    }
+}
