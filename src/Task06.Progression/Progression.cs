3db047eee0ca37c7fd89b82589fdd74fe61cbc37
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-06 18:18:55 +0300

    улучшила программу для арифметической прогрессии

--- src/Task06.Progression/Progression.cs
+++ src/Task06.Progression/Progression.cs
@@ -10,16 +10,43 @@ namespace Task06.Progression
     {
         public static void Main()
         {
-            int i0 = 1;
-            int numberCount = 15;
-            for (int i = 0; i < numberCount; i++)
+            Console.WriteLine("напишите исходное число");
+            string str1 = Console.ReadLine();
+            int number1;
+            if (!int.TryParse(str1, out number1))
             {
-                Console.WriteLine(2*i+1);
+                Console.WriteLine("надо написать число");
+                Console.ReadLine();
+                return;
             }
 
+            Console.WriteLine("введите шаг");
+            string str2 = Console.ReadLine();
+            int step;
+            if (!int.TryParse(str2, out step))
+            {
+                Console.WriteLine("надо написать число");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("напишите количество чисел");
+            string str3 = Console.ReadLine();
+            int count;
+            if (!int.TryParse(str3, out count))
+            {
+                Console.WriteLine("надо написать число");
+                Console.ReadLine();
+                return;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(step*number1 + i);
+            }
+
+
             Console.ReadLine();
-
-
         }
     }
 }
