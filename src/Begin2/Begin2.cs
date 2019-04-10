9a71baf0198e7e47905e9bb027c01532a107a542
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-10 22:06:51 +0300

    решила задачу begin2

---
+++ src/Begin2/Begin2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin2
+{
+    class Begin2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону квадрата");
+            int size = int.Parse(Console.ReadLine());
+            if (size < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число. Сторона квадрата не может быть отрицательной");
+                return;
+            }
+
+            int square = size * size;
+            Console.WriteLine($"S={square}");
+            Console.ReadLine();
+        }
+    }
+}
