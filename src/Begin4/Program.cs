24b769e4eec49653ff9556b09d5be9634b19ec2e
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-10 22:18:02 +0300

    решила задачу Begin4

---
+++ src/Begin4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin4
+{
+    class Begin4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диаметр");
+            double diameter = double.Parse(Console.ReadLine());
+            if (diameter < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число. Диаметр не может быть отрицательным");
+                return;
+            }
+
+            double p = 3.14;
+            double length = p * diameter;
+            Console.WriteLine($"L={length}");
+            Console.ReadLine();
+        }
+    }
+}
