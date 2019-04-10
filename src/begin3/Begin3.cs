d5847a7adf04e84a0be1ca473a814730a4346e51
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-10 22:07:19 +0300

    решила задачу Begin3

---
+++ src/begin3/Begin3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace begin3
+{
+    class Begin3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую сторону прямоугольника");
+            int number = int.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число. Сторона прямоугольника не может быть отрицательной");
+                return;
+            }
+            Console.WriteLine("Введите вторую сторону прямоугольника");
+            int count = int.Parse(Console.ReadLine());
+            if (count < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число. Сторона прямоугольника не может быть отрицательной");
+                return;
+            }
+
+            int perimeter = (number  + count)*2;
+            Console.WriteLine($"P={perimeter}");
+
+            int square = number*count;
+            Console.WriteLine($"S={square}");
+            Console.ReadLine();
+        }
+    }
+}
