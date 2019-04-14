d835d987c95f8c16fd3f718e010e0d6b8d1a8c69
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-14 14:29:17 +0300

    Решила задачу Begin14

---
+++ src/Begin14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double radius = double.Parse(Console.ReadLine());
+            if (radius  < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число. Радиус не может быть отрицательным");
+                return;
+            }
+
+            double p = 3.14;
+            double length = 2 * p * radius;
+            Console.WriteLine($"L={length}");
+            double square = p * radius * radius;
+            Console.WriteLine($"S={square}");
+
+            Console.ReadLine();
+        }
+    }
+}
