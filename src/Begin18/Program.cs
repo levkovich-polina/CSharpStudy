ea1ccbf1ab79fba0105984847f6391b66596c9a8
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-16 22:57:06 +0300

    решила задачу Begin18

---
+++ src/Begin18/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону A");
+            int side1 = int.Parse(Console.ReadLine());
+            if (side1 < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число. Сторона не может быть отрицательным");
+                return;
+            }
+
+            Console.WriteLine("Введите сторону C");
+            int side2 = int.Parse(Console.ReadLine());
+            if (side2 < side1)
+            {
+                Console.WriteLine("Вы ввели число меньше. Сторона должна быть больше стороны A");
+                return;
+            }
+
+            Console.WriteLine("Введите сторону B");
+            int side3 = int.Parse(Console.ReadLine());
+            if (side3 < side2)
+            {
+                Console.WriteLine("Вы  ввели число меньше. Сторона должна быть больше стороны C");
+                return;
+            }
+
+           
+
+            int a = side2 - side1;
+            Console.WriteLine($"AC={a}");
+            int b = side3 - side2;
+            Console.WriteLine($"BC={b}");
+
+            Console.ReadLine();
+        }
+    }
+}
