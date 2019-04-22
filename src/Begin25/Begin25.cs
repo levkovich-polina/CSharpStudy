77cbf1e5c40b34504623bf6e413e407b74db082b
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-22 20:57:16 +0300

    исправили задачу Begin25

--- src/Begin25/Begin25.cs
+++ src/Begin25/Begin25.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace Begin25
 {
@@ -14,10 +9,9 @@ namespace Begin25
             Console.WriteLine("Введите число x");
             double x = double.Parse(Console.ReadLine());
 
-            double Y = 3*x*x*x*x*x*x - 6*x*x -7 ;
-            Console.WriteLine($"y={Y}");
+            double y = 3 * x * x * x * x * x * x - 6 * x * x - 7;
+            Console.WriteLine($"y={y}");
             Console.ReadLine();
-
         }
     }
 }
