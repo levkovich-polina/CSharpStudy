37c1a4db50e17d8c31cc39fd392084083e67888b
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-22 17:20:29 +0300

    решила задачу Begin25

---
+++ src/Begin25/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Begin25
+{
+    class Begin25
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число x");
+            double x = double.Parse(Console.ReadLine());
+
+            double Y = 3*x*x*x*x*x*x - 6*x*x -7 ;
+            Console.WriteLine($"y={Y}");
+            Console.ReadLine();
+
+        }
+    }
+}
