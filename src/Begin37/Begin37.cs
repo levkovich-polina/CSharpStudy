6b90f5cea64c01eb637533af870d8d824c9ca425
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-22 21:12:19 +0300

    исправила задачу Begin37

--- src/Begin37/Begin37.cs
+++ src/Begin37/Begin37.cs
@@ -11,39 +11,43 @@ namespace Begin37
         static void Main(string[] args)
         {
             Console.WriteLine("Введите скорость первого автомобиля");
-            double speed = double.Parse(Console.ReadLine());
-            if (speed < 0)
-            {
-                Console.WriteLine("Вы ввели отрицательную скорость.");
-                return;
-            }
-            Console.WriteLine("Введите скорость второго автомобиля");
             double speed1 = double.Parse(Console.ReadLine());
             if (speed1 < 0)
             {
-                Console.WriteLine("Вы ввели отрицательную скорость.");
+                Console.WriteLine("Вы ввели отрицательную скорость Скорость автомобиля не должна быть отрицательной.");
                 return;
             }
-            Console.WriteLine("Введите расстояние между автомобилями");
-            double distances = double.Parse(Console.ReadLine());
-            if (distances < 0)
+
+            Console.WriteLine("Введите скорость второго автомобиля");
+            double speed2 = double.Parse(Console.ReadLine());
+            if (speed2 < 0)
             {
-                Console.WriteLine("Вы ввели отрицательное расстояние.");
+                Console.WriteLine("Вы ввели отрицательную скорость. Скорость автомобиля не должна быть отрицательной");
                 return;
             }
+
+            Console.WriteLine("Введите первоначальное расстояние между автомобилями");
+            double startDistance = double.Parse(Console.ReadLine());
+            if (startDistance < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное расстояние. Первоначальное расстояние не должно быть отрицательным");
+                return;
+            }
+
             Console.WriteLine("Введите время");
             double time = double.Parse(Console.ReadLine());
             if (time < 0)
             {
-                Console.WriteLine("Вы ввели отрицательное время.");
+                Console.WriteLine("Вы ввели отрицательное время. Время не должно быть отрицательным");
                 return;
             }
 
-            double distances1 = time * (speed + speed1);
-            double distances2 = Math.Abs(distances - distances1);
-            Console.WriteLine($"S={distances2}");
+            double distance1 = time * speed1;
+            double distance2 = time * speed2;
+            double sumCarDistance = distance1 + distance2;
+            double finalDistance = Math.Abs(startDistance - sumCarDistance);
+            Console.WriteLine($"Final distance={finalDistance}");
             Console.ReadLine();
-
         }
     }
 }
