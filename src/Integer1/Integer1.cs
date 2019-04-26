2376ac18ff38b21f47b4be25e22019e36e403764
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-26 21:07:03 +0300

    решила задачу Integer1

---
+++ src/Integer1/Integer1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer1
+{
+    class Integer1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите расстояние в сантиметрах");
+            int l = int.Parse(Console.ReadLine());
+            if (l < 0)
+            {
+                Console.WriteLine("Расстояние не может быть отрицательным");
+                return;
+            }
+
+            int lenght = l / 100;
+            Console.WriteLine($"{l} сантиметров = {lenght} метров");
+            Console.ReadLine();
+
+        }
+    }
+}
