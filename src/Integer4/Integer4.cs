0a273c32105a51ccde7d2e6f9e299f3a4b3f798c
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-26 21:25:40 +0300

    решила задачу Integer4

---
+++ src/Integer4/Integer4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer4
+{
+    class Integer4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            int a = int.Parse(Console.ReadLine());
+            if (a < 0)
+            {
+                Console.WriteLine("Число A не должен быть равен 0");
+                return;
+            }
+            Console.WriteLine("Введите число B");
+            int b = int.Parse(Console.ReadLine());
+            if (b > a)
+            {
+                Console.WriteLine("Число B должно быть меньше числа A");
+                return;
+            }
+
+            int ab = a / b;
+            Console.WriteLine($"{ab} целых отрезков");
+            Console.ReadLine();
+        }
+    }
+}
