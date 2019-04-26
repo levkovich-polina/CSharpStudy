c314d996f4b8a0e197907c306fe2e95e92e8aded
Levkovich Polina <levkovich.polina@gmail.com>
2019-04-26 21:15:12 +0300

    решила задачу Integer3

---
+++ src/Integer3/Integer3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer3
+{
+    class Integer3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер файла в байтах");
+            int b = int.Parse(Console.ReadLine());
+            if (b < 0)
+            {
+                Console.WriteLine("Размер файла не может быть отрицательным");
+                return;
+            }
+
+            int kB = b / 1024;
+            Console.WriteLine($"{b} байта= {kB} килобайт");
+            Console.ReadLine();
+
+        }
+    }
+}
