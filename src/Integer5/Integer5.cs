19cb24c82f2c82fc685b867d25b3dbaf401e81d4
Levkovich Polina <levkovich.polina@gmail.com>
2019-05-05 23:56:29 +0300

    решила задачу Integer5

---
+++ src/Integer5/Integer5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Integer5
+{
+    class Integer5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            double a = double.Parse(Console.ReadLine());
+            if (a < 0)
+            {
+                Console.WriteLine("Число A должно быть неотрицательным");
+                return;
+            }
+            Console.WriteLine("Введите число B");
+            double b = double.Parse(Console.ReadLine());
+            if (b > a)
+            {
+                Console.WriteLine("Число B должно быть меньше числа A");
+                return;
+            }
+
+            if (b == 0)
+            {
+                Console.WriteLine("Число B не должно быть равно 0");
+                return;          
+            }
+
+            double remainder = a % b;
+            Console.WriteLine($"Остаток = {remainder}");
+            Console.ReadLine();
+        }
+    }
+}
