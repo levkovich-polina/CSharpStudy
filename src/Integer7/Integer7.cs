d74c7a9dbeba03bda2088521fc52adb4afadeb98
Levkovich Polina <levkovich.polina@gmail.com>
2019-05-06 00:36:05 +0300

    решила задачу Integer7

---
+++ src/Integer7/Integer7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer7
+{
+    class Integer7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите двухзначное число");
+            int number;
+            number = int.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+            if (number < 10 || number >= 100 )
+            {
+                Console.WriteLine("Число должно быть двухзначным");
+                Console.ReadLine();
+                return;
+            }
+            
+            int tens = Math.Abs(number / 10);
+            int units = Math.Abs(number % 10);
+            int sum = tens + units;
+            int multiplication = tens * units;
+            Console.WriteLine($"{sum} сумма двух цирф");
+            Console.WriteLine($"{multiplication} произведение двух цифр");
+            Console.ReadLine();
+        }
+    }
+}
