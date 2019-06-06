ccb0d225018becaf7812cdf1d110ee4d4fe7f7df
Levkovich Polina <levkovich.polina@gmail.com>
2019-06-06 22:01:15 +0300

    решила задачу Integer21

---
+++ src/Integer21/Integer21.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer21
+{
+    class Integer21
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество секунд");
+            var totalSeconds = int.Parse(Console.ReadLine());
+            if (totalSeconds < 0)
+            {
+                Console.WriteLine("Число должно быть неотрицательным");
+                Console.ReadLine();
+                return;
+            }
+
+            int seconds = totalSeconds % 60;
+            Console.WriteLine($"{seconds} секунд прошло с последней минуты");
+            Console.ReadLine();
+        }
+    }
+}
