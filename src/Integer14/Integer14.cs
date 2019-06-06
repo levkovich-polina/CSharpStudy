f81027c770cb25abe47ee6f3618ceada31dc8699
Levkovich Polina <levkovich.polina@gmail.com>
2019-06-06 21:49:11 +0300

    решила задачу Integer14

---
+++ src/Integer14/Integer14.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Integer14
+{
+    class Integer14
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трёхзнчное число");
+            var number = int.Parse(Console.ReadLine());
+            if (number < 100 || number >= 1000)
+            {
+                Console.WriteLine("Число должно быть трёхзначным");
+                Console.ReadLine();
+                return;
+            }
+
+            int hundreds = number / 100;
+            int tens = number / 10 % 10;
+            int units = number % 10;
+            int sum = (units * 100) + (hundreds * 10) + tens;
+            Console.WriteLine($"{sum}");
+            Console.ReadLine();
+        }
+    }
+}
