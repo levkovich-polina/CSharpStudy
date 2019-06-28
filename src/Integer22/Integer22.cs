bc6343e680aeeeba882b374cc88349f32d8eec6c
Levkovich Polina <levkovich.polina@gmail.com>
2019-06-28 22:32:29 +0300

    решила задачу Integer22

---
+++ src/Integer22/Integer22.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer22
+{
+    class Integer22
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество секунд");
+            var totalSeconds = int.Parse(Console.ReadLine());
+            if (totalSeconds < 0)
+            {
+                Console.WriteLine("Число должно быть неотрицательным");
+                Console.ReadLine();
+                return;
+            }
+            int seconds = totalSeconds % 360;
+            Console.WriteLine($"{seconds} секунд прошло с последнего часа");
+            Console.ReadLine();
+        }
+    }
+}
