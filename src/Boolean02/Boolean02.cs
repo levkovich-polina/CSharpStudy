65a791c7cb848523cb74138839b1fab7ea68fb1e
Levkovich Polina <levkovich.polina@gmail.com>
2019-07-11 17:23:42 +0300

    решила задачу Boolean02
    #2

---
+++ src/Boolean02/Boolean02.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Boolean02
+{
+    class Boolean02
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число");
+            var number = int.Parse(Console.ReadLine());
+
+            bool isNumberOdd = ((number % 2) == 1);
+            Console.WriteLine($"Число {number} является нечетным? - {isNumberOdd}");
+            Console.ReadLine();
+        }
+    }
+}
