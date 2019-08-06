f6f980a5dd75dd9098f22218dcd9279c26cb8622
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-06 17:03:12 +0300

    решила задачу Boolean09
    #4

---
+++ src/Boolean09/Boolean09.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Boolean09
+{
+    class Boolean09
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число A");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = int.Parse(Console.ReadLine());
+
+            bool isAOdd = (a % 2) == 1;
+            bool isBOdd = (b % 2) == 1;
+            bool isAnyNumberOdd = isBOdd || isAOdd;
+        
+            Console.WriteLine($"Хотя бы одно из чисел A и B нечетное?  - {isAnyNumberOdd}");
+            Console.ReadLine();
+        }
+        
+    }
+}
