698e33cc6ad71aa1c61b3259a7110205f2feb43a
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-06 16:51:23 +0300

    решила задачу Boolean06
    #3

---
+++ src/Boolean6/Boolean6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Boolean6
+{
+    class Boolean6
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число A");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число C");
+            var c = int.Parse(Console.ReadLine());
+
+            bool isInequalityTrue = ((a < b) && (b < c));
+            Console.WriteLine($"Справедливо двойное неравенство A<B<C? - {isInequalityTrue}");
+            Console.ReadLine();
+        }
+    }
+}
