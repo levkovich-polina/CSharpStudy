84dd4c6ac066764c63fe03ee549927a097fd1baa
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-06 17:22:21 +0300

    решила задачу Boolean12
    #5

---
+++ src/Boolean12/Boolean12.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Boolean12
+{
+    class Boolean12
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число A");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите число C");
+            var c = int.Parse(Console.ReadLine());
+
+            bool isAOdd = a > 0;
+            bool isBOdd = b > 0;
+            bool isCOdd = c > 0;
+            bool areAllNumbersPositive = isAOdd && isBOdd && isCOdd;
+
+            Console.WriteLine($"Является каждое из чисел положительным?  - {areAllNumbersPositive}");
+            Console.ReadLine();
+        }
+    }
+}
