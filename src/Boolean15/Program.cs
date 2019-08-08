c500e61f0291ea545bd118eaaaf0ad4c5b0e945e
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-08 22:44:38 +0300

    решила задачу Boolean15
    #7

---
+++ src/Boolean15/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Boolean15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число A");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число C");
+            var c = int.Parse(Console.ReadLine());
+
+            bool isAPositive = a > 0;
+            bool isBPositive = b > 0;
+            bool isCPositive = c > 0;
+
+            int positiveNumberCount = 0;
+            if (isAPositive)
+            {
+                positiveNumberCount = positiveNumberCount + 1;
+            }
+
+            if (isBPositive)
+            {
+                positiveNumberCount = positiveNumberCount + 1;
+            }
+
+            if (isCPositive)
+            {
+                positiveNumberCount = positiveNumberCount + 1;
+            }
+
+            bool onlyTwoNumbersArePositive = positiveNumberCount == 2;
+            Console.WriteLine($"Являются только два числа из A,B,C положительными?- {onlyTwoNumbersArePositive}");
+            Console.ReadLine();
+        }
+    }
+}
