b42ee6f792a2ca5df0e8cc5d2754b5f82294012b
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-12 17:52:49 +0300

    решила задачу Boolean19
    #9

---
+++ src/Boolean19/Boolean19.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Boolean19
+{
+    class Boolean19
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число A");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число C");
+            var c = int.Parse(Console.ReadLine());
+
+
+            bool areMutuallyOppositeNumbersExist = (a + b == 0) || (b + c == 0) || (a + c == 0);
+            Console.WriteLine($"Существуют ли среди трёх чисел пара взаимно противоположных? - {areMutuallyOppositeNumbersExist}");
+            Console.ReadLine();
+        }
+    }
+}
