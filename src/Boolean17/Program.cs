18df21952ead6baa49b98af7dede3f2b074d055a
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-12 16:42:23 +0300

    решила задачу Boolean17
    #8

---
+++ src/Boolean17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Boolean17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите положительное число");
+            var a = int.Parse(Console.ReadLine());
+            bool theNumberIsThreeDigitAndPositive = ((a > 99) && ((a % 2 )==0));
+            Console.WriteLine($"Является число нечетным и положительным?-{theNumberIsThreeDigitAndPositive}");
+            Console.ReadLine();
+
+        }
+    }
+}
