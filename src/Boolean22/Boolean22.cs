cdf9c41b7252c68673d3024eca08c2ffb68f7ffa
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-12 18:29:47 +0300

    решила задачу Boolean22
    #11

---
+++ src/Boolean22/Boolean22.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Boolean22
+{
+    class Boolean22
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите трёхзначное число");
+            var number = int.Parse(Console.ReadLine());
+            if (number < 100 || number > 999)
+            {
+                Console.WriteLine("Нужно ввести трёхзначное число");
+                Console.ReadLine();
+                return;
+            }
+            int hundreds = number / 100;
+            int tens = (number % 100) / 10;
+            int units = number % 10;
+            bool doDigitsFormIncreasingSequence = (hundreds < tens) && (tens < units);
+            bool doDigitsFormDecreasingSequence = (hundreds > tens) && (tens > units);
+            bool doDigitsFormIncreasingOrDecreasingSequence = doDigitsFormDecreasingSequence || doDigitsFormIncreasingSequence;
+            Console.WriteLine($"Образуют ли цифры возрастающую или убывающую последовательность?-{doDigitsFormIncreasingOrDecreasingSequence}");
+            Console.ReadLine();
+
+        }
+    }
+}
