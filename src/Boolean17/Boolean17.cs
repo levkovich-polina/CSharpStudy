d03b1c84e4efe3c966c185e8e865a4537c6fc5ee
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-12 17:52:16 +0300

    исправила ошибки в задаче Boolean17
    #8

---
+++ src/Boolean17/Boolean17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Boolean17
+{
+    class Boolean17
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите положительное число");
+            var a = int.Parse(Console.ReadLine());
+            bool isNumberOdd = ((a % 2) == 1);
+            bool isNumberThreeDigit = (a > 99) && (a < 1000);
+            bool isNumberThreeDigitAndOdd = (isNumberThreeDigit && isNumberOdd);
+            Console.WriteLine($"Является число нечетным и трёхзначным?-{isNumberThreeDigitAndOdd}");
+            Console.ReadLine();
+        }
+    }
+}
