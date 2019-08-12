ec96dd97b5038480cb4866ca55a61b8b22657df6
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-12 18:21:20 +0300

    решила задачу Boolean21
    #10

---
+++ src/Boolean21/Boolean21.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Boolean21
+{
+    class Boolean21
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите трёхзначное число");
+            var number = int.Parse(Console.ReadLine());
+            if (number < 100 || number > 999)
+            {
+                Console.WriteLine("Нужно ввести трёхзначное число");
+                Console.ReadLine();
+                return;
+            }
+            int hundreds = number / 100;
+            int tens = (number % 100) / 10;
+            int units = number % 10;
+            bool doDigitsFormIncreasingSequence = (hundreds < tens) && (tens < units);
+            Console.WriteLine($"Образуют ли цифры возрастающую последовательность?-{doDigitsFormIncreasingSequence}");
+            Console.ReadLine();
+
+        }
+    }
+}
