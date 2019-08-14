1e5765cf6cbe7b74df66830d8765cad6ff708dd8
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-14 17:10:17 +0300

    решила задачу Boolean30
    #15

---
+++ src/Boolean30/Boolean30.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Boolean30
+{
+    class Boolean30
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите сторону a");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону b");
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону c");
+            var c = double.Parse(Console.ReadLine());
+
+            bool isTriangleEquilateral = (a == b) && (b == c);
+            Console.WriteLine($"Является треугольник равносторинним?-{isTriangleEquilateral}");
+            Console.ReadLine();
+        }
+    }
+}
