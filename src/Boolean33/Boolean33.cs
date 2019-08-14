90845cb8a96463413f8e34c17cdbab0cc8d971b5
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-14 17:43:27 +0300

    решила задачу Boolean33
    #17

---
+++ src/Boolean33/Boolean33.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Boolean33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите сторону a");
+            var a = double.Parse(Console.ReadLine());
+            if (a <= 0)
+            {
+                Console.WriteLine("Нужно ввести положительное число");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Введите сторону b");
+            var b = double.Parse(Console.ReadLine());
+            if (b <= 0)
+            {
+                Console.WriteLine("Нужно ввести положительное число");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Введите сторону c");
+            var c = double.Parse(Console.ReadLine());
+            if (c <= 0)
+            {
+                Console.WriteLine("Нужно ввести положительное число");
+                Console.ReadLine();
+                return;
+            }
+
+            bool canTriangleExist = (a + b > c) && (a + c > b) && (b + c > a);
+            Console.WriteLine($"Треугольник существует?-{canTriangleExist}");
+            Console.ReadLine();
+        }
+    }
+}
