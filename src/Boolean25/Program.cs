0c579414b294a4dc3096950d7d7e00709b343243
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-14 13:53:32 +0300

    решила задачу Boolean25
    #12

---
+++ src/Boolean25/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Boolean25
+{
+    class Boolean25
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число X");
+            var x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число Y");
+            var y = int.Parse(Console.ReadLine());
+
+            bool pointWithCoordinateXYLiesInTheSecondCoordinateQuarter = (x < 0) && (y > 0);
+            Console.WriteLine($"Точка с координатами (x,y) лежит во второй координатной четверти?-{pointWithCoordinateXYLiesInTheSecondCoordinateQuarter}");
+            Console.ReadLine();
+
+        }
+    }
+}
