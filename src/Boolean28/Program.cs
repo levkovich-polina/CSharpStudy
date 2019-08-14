d7aad8c3c1eea4a0a408cf486c10eda388969166
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-14 14:18:57 +0300

    решила задачу Boolean28
    #13

---
+++ src/Boolean28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Boolean28
+{
+    class Boolean28
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число X");
+            var x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число Y");
+            var y = int.Parse(Console.ReadLine());
+
+            bool pointWithCoordinateXYLiesinTheFirstOrThirdCoordinateQuarter = ((x < 0) && (y < 0))||((x>0)&&(y>0));
+            Console.WriteLine($"Точка с координатами (x,y) лежит в первой или третьей координатной четверти?-{pointWithCoordinateXYLiesinTheFirstOrThirdCoordinateQuarter}");
+            Console.ReadLine();
+
+        }
+    }
+}
