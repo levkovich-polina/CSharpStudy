48d760b59a16179bfe9f8cde440e7323c68931a4
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-15 21:56:37 +0300

    решила задачу Boolean36
    #19

---
+++ src/Boolean36/Boolean36.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Boolean36
+{
+    class Boolean36
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число x1");
+            var x1 = int.Parse(Console.ReadLine());
+            if (x1 <= 0 || x1 >= 9)
+            {
+                Console.WriteLine("Координата поля шахматной доски должна лежат в интервале от 1 до 8");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Введите число y1");
+            var y1 = int.Parse(Console.ReadLine());
+            if (y1 <= 0 || y1 >= 9)
+            {
+                Console.WriteLine("Координата поля шахматной доски должна лежат в интервале от 1 до 8");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Введите число x2");
+            var x2 = int.Parse(Console.ReadLine());
+            if (x2 <= 0 || x2 >= 9)
+            {
+                Console.WriteLine("Координата поля шахматной доски должна лежат в интервале от 1 до 8");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Введите число y2");
+            var y2 = int.Parse(Console.ReadLine());
+            if (y2 <= 0 || y2 >= 9)
+            {
+                Console.WriteLine("Координата поля шахматной доски должна лежат в интервале от 1 до 8");
+                Console.ReadLine();
+                return;
+            }
+
+            bool canRookMakeTurn = (x1 == x2) || (y2 == y1);
+            Console.WriteLine($"Может ли ладья за один ход перейти с одного поля на другое?-{canRookMakeTurn}");
+            Console.ReadLine();
+        }
+    }
+}
