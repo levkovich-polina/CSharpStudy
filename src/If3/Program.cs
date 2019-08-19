adc63e902fc61ec330cf43ce9f22535334d9e34a
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-19 21:14:40 +0300

    решила задачу If3
    #23

---
+++ src/If3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace If3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число");
+            var x = double.Parse(Console.ReadLine());
+            if (x > 1)
+            {
+                x = x + 1;
+            }
+
+            if (x < -1)
+            {
+                x = x - 2;
+            }
+
+            if (x == 0)
+            {
+                x = 10;
+            }
+
+            Console.WriteLine($"Результат:{x}");
+            Console.ReadLine();
+        }
+    }
+}
