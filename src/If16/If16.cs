af69fa33f1cb135715d7f28b1476533d7a2cfa07
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-27 21:52:25 +0300

    решила задачу If16
    #26

---
+++ src/If16/If16.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace If16
+{
+    class If16
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число A");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число C");
+            var c = double.Parse(Console.ReadLine());
+            if (a < b && b < c)
+            {
+                a = a * 2;
+                b = b * 2;
+                c = c * 2;
+            }
+            else
+            {
+                a = -a;
+                b = -b;
+                c = -c;
+            }
+
+            Console.WriteLine($"A={a}");
+            Console.WriteLine($"B={b}");
+            Console.WriteLine($"C={c}");
+            Console.ReadLine();
+        }
+    }
+}
