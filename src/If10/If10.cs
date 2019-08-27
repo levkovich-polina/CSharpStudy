05138ccad7eaa8d582456bbaaaf1c632a84e5314
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-27 21:44:24 +0300

    решила задачу If10
    #25

---
+++ src/If10/If10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace If10
+{
+    class If10
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число A");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = int.Parse(Console.ReadLine());
+
+            if (a != b)
+            {
+                int sum = a + b;
+                a = sum;
+                b = sum;
+            }
+            else
+            {
+                a = 0;
+                b = 0;
+            }
+
+            Console.WriteLine($"A={a}");
+            Console.WriteLine($"B={b}");
+            Console.ReadLine();
+        }
+    }
+}
