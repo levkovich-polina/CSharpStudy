a4a474cd0428f993609639d275a48db6dddc0d84
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-27 21:27:00 +0300

    решила задачу If9
    #24

---
+++ src/If9/If9.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace If9
+{
+    class If9
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите число A");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = double.Parse(Console.ReadLine());
+
+            double c;
+
+            if (a > b)
+            {
+                c = b;
+                b = a;
+                a = c;
+            }
+
+            Console.WriteLine($"Меньшее значение - {a};");
+            Console.WriteLine($"Большее значение - {b}");
+            Console.ReadLine();
+        }
+    }
+}
