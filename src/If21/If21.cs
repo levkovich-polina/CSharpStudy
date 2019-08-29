dd3be0d0e48a52589450f9f922657812612c7aff
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-29 21:13:56 +0300

    решила задачу If21
    #28

---
+++ src/If21/If21.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace If21
+{
+    class If21
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите координату X");
+            var x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координату Y");
+            var y = int.Parse(Console.ReadLine());
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine($"0");
+                Console.ReadLine();
+            }
+            else if (y == 0)
+            {
+                Console.WriteLine($"1");
+                Console.ReadLine();
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine($"2");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"3");
+                Console.ReadLine();
+            }
+        }
+    }
+}
