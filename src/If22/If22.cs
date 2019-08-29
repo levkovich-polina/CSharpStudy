57354b33849807ed7e63b0258f52ed22ca1512f2
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-29 21:30:11 +0300

    решила задачу If22
    #29

---
+++ src/If22/If22.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace If22
+{
+    class If22
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите координату X");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координату Y");
+            var y = double.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine($"1 координатная четверть");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine($"2 координатная четверть");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine($"3 координатная четверть");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine($"4 координатная четверть");
+            }
+            else
+            {
+                Console.WriteLine($"Точка лежит на координатной оси");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
