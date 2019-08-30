13f162629d65dd3e8196eaed10c8a9c239bba2f6
Levkovich Polina <levkovich.polina@gmail.com>
2019-08-30 23:42:53 +0300

    решила задачу If26
    #32

---
+++ src/If26/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace If26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите X");
+            var x = double.Parse(Console.ReadLine());
+
+            if (x <= 0)
+            {
+                double x1;
+                x1 = -x;
+                Console.WriteLine($"F(x)={x1}");
+            }
+            else if (0 < x && x < 2)
+            {
+                double x2;
+                x2 = x * x;
+                Console.WriteLine($"F(x)={x2}");
+            }
+            else
+            {
+                Console.WriteLine($"F(x)=4");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
