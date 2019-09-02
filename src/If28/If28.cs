86e3df3c1e9ec982d996a30a5ae00c057bc8d7e7
Levkovich Polina <levkovich.polina@gmail.com>
2019-09-02 23:21:46 +0300

    решила задачу If28
    #33

---
+++ src/If28/If28.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace If28
+{
+    class If28
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер года");
+            var year = int.Parse(Console.ReadLine());
+            if (year <= 0)
+            {
+                Console.WriteLine("Число должно быть положительным");
+                Console.ReadLine();
+                return;
+            }
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine("Это високосный год, в нем 366 дней");
+            }
+            else
+            {
+                Console.WriteLine("Это обычный год, в нем 365 дней");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
