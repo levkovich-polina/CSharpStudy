84b653b2a0dff4ea557445ade79348b32ce961e2
Levkovich Polina <levkovich.polina@gmail.com>
2019-09-02 22:14:01 +0300

    решила задачу If24
    #31

---
+++ src/If24/If24.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace If24
+{
+    class If24
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите X");
+            var x = double.Parse(Console.ReadLine());
+            if (x > 0)
+            {
+                double f = 2 * Math.Sin(x);
+                Console.WriteLine($"F(x)={f}");
+            }
+            else
+            {
+                double f = 6 - x;
+                Console.WriteLine($"F(x)={f}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
