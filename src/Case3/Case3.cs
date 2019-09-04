6abbcd2d208190fa15d8d431e8e0467522b6fa7e
Levkovich Polina <levkovich.polina@gmail.com>
2019-09-04 21:23:44 +0300

    решила задачу Case3
    #36

---
+++ src/Case3/Case3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Case3
+{
+    class Case3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца");
+            var month = int.Parse(Console.ReadLine());
+
+            if (month == 1 || month == 2 || month == 12)
+            {
+                Console.WriteLine("Зима");
+            }
+            else if (month == 3 || month == 4 || month == 5)
+            {
+                Console.WriteLine("Весна");
+            }
+            else if (month == 6 || month == 7 || month == 8)
+            {
+                Console.WriteLine("Лето");
+            }
+            else if (month == 9 || month == 10 || month == 11)
+            {
+                Console.WriteLine("Осень");
+            }
+            else
+            {
+                Console.WriteLine($"Времени года, соответвующему месяцу \"{month}\", не существует");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
