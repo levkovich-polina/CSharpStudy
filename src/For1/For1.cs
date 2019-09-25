1d7c3bec828546238a4ef9ac930384b1523ba5e7
Levkovich Polina <levkovich.polina@gmail.com>
2019-09-25 19:25:21 +0300

    решила задачу For1
    #43

---
+++ src/For1/For1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace For1
+{
+    class For1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число K");
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число N");
+            var count = int.Parse(Console.ReadLine());
+
+            if (count <= 0)
+            {
+                Console.WriteLine("N должно быть больше 0");
+                return;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
