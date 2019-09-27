f4e95561467dddf23cc290d68801dbb24f1e5b79
Levkovich Polina <levkovich.polina@gmail.com>
2019-09-27 18:03:07 +0300

    решила задачу For2
    #44

---
+++ src/For2/For2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace For2
+{
+    class For2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число B");
+            var b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Число A должно быть меньше числа B");
+                return;
+            }
+
+            for (int i = 0; i <= b - a; i++)
+            {
+                Console.WriteLine(a + i);
+            }
+
+            int sum = b - a + 1;
+
+            Console.WriteLine($"{sum} чисел");
+
+            Console.ReadLine();
+        }
+    }
+}
