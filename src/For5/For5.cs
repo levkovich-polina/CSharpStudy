c1ddf4b4da4e0b11663a70e81933e5e52919c762
Levkovich Polina <levkovich.polina@gmail.com>
2019-10-03 22:26:50 +0300

    решила задачу For5
    #47

---
+++ src/For5/For5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace For5
+{
+    class For5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите цену за 1 кг");
+            var price = double.Parse(Console.ReadLine());
+            if (price <= 0)
+            {
+                Console.WriteLine("Число должно быть больше 0");
+                return;
+            }
+
+            for (double i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{price*(i/10)} рублей за {i/10} кг");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
