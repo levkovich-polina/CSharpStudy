d998491eeb3a850df868b1d9ae3ca65963bc3e24
Levkovich Polina <levkovich.polina@gmail.com>
2019-10-12 15:08:24 +0300

    решила задачу For10
    #49

---
+++ src/For10/For10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace For10
+{
+    class For10
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N");
+            var n = int.Parse(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("Число N должно быть больше 0");
+                return;
+            }
+
+            double sum = 0;
+            for (double i = 1; i <= n; i++)
+            {
+                sum = sum + (1/ i);
+                Console.WriteLine(sum);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
