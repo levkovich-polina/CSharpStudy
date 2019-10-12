7fd1451f3439876ac0a8006709b90f4d9e088e09
Levkovich Polina <levkovich.polina@gmail.com>
2019-10-12 14:47:29 +0300

    решила задачу For11
    #50

---
+++ src/For11/For11.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace For11
+{
+    class For11
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N");
+            var n = int.Parse(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("Число N должно быть больше 0");
+                return;
+            }
+
+            double sum = 0;
+            for (double i = 0; i <= n; i++)
+            {
+                sum = sum +(n+i)*(n + i);
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
