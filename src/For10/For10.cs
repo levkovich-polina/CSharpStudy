26c109bfa9a5c5616c4bf5bcb1ccea6cc2502d00
Levkovich Polina <levkovich.polina@gmail.com>
2019-10-16 20:54:20 +0300

    #49

--- src/For10/For10.cs
+++ src/For10/For10.cs
@@ -6,7 +6,7 @@ namespace For10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число N");
+            Console.WriteLine("Введите целое число N");
             var n = int.Parse(Console.ReadLine());
 
             if (n < 0)
@@ -15,8 +15,8 @@ namespace For10
                 return;
             }
 
-            double sum = 0;
-            for (double i = 1; i <= n; i++)
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
             {
                 sum = sum + (1/ i);
                 Console.WriteLine(sum);
