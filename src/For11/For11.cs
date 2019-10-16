0030500dfcc7f25369d57418cfad0d7026b22aa3
Levkovich Polina <levkovich.polina@gmail.com>
2019-10-16 20:54:36 +0300

    #50

--- src/For11/For11.cs
+++ src/For11/For11.cs
@@ -6,7 +6,7 @@ namespace For11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число N");
+            Console.WriteLine("Введите целое число N");
             var n = int.Parse(Console.ReadLine());
 
             if (n < 0)
@@ -15,8 +15,8 @@ namespace For11
                 return;
             }
 
-            double sum = 0;
-            for (double i = 0; i <= n; i++)
+            int sum = 0;
+            for (int i = 0; i <= n; i++)
             {
                 sum = sum +(n+i)*(n + i);
             }
