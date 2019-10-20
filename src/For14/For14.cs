5cfa13ae68f9b8aa5171ac63ca4de87472a43bf6
Levkovich Polina <levkovich.polina@gmail.com>
2019-10-20 16:32:44 +0300

    решила задачу For14
    #51

---
+++ src/For14/For14.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace For14
+{
+    class For14
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N");
+            var n = int.Parse(Console.ReadLine());
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Число N должно быть больше 0");
+                return;
+            }
+
+            int square = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                square = square + (2 * i + 1);
+            }
+
+            Console.WriteLine(square);
+            Console.ReadLine();
+        }
+    }
+}
