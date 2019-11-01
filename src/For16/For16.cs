9387727817b3c2259d240ebcc62e3c17ea23d7af
Levkovich Polina <levkovich.polina@gmail.com>
2019-11-02 00:42:12 +0300

    решила задачу For16
    #52

---
+++ src/For16/For16.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace For16
+{
+    class For16
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите целое число N");
+            var n = int.Parse(Console.ReadLine());
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Число N должно быть больше 0");
+                return;
+            }
+
+            double number = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                number = number*a;
+            }
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
