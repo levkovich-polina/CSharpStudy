4637ca1e4d0e78a020063cae38b126c42a888eb1
Levkovich Polina <levkovich.polina@gmail.com>
2019-11-03 13:52:59 +0300

    решила задачу For19
    #54

---
+++ src/For19/For19.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace For19
+{
+    class For19
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N");
+            var n = int.Parse(Console.ReadLine());
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Число N должно быть больше 0");
+                return;
+            }
+
+            double factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+                Console.WriteLine($"{i}! = {factorial}");
+            }
+
+            Console.WriteLine($"Итоговый факториал числа {n} = {factorial}");
+            Console.ReadLine();
+        }
+    }
+}
