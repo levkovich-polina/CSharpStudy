725bc28e1558e7e0192058ef6b97ed5bffb834fb
Levkovich Polina <levkovich.polina@gmail.com>
2019-12-28 21:40:42 +0300

    решила задачу Array01
    #77

---
+++ src/Array01/Array01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array01
+{
+    class Array01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N ");
+            var n = int.Parse(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("Число N должно быть больше 0");
+                return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int a;
+                a = 2 * i + 1;
+                Console.WriteLine($"x[{i}] = {a}");
+            }
+        }
+    }
+}
