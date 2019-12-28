48714716e0aab45f1e41f4e55d9cafb6a43cdf64
Levkovich Polina <levkovich.polina@gmail.com>
2019-12-28 22:34:39 +0300

    решила задачу Array02
    #80

---
+++ src/Array2/Array02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Array2
+{
+    class Array02
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N ");
+            var n = int.Parse(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("Число N должно быть больше 0");
+                return;
+            }
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i}й элемент массивва");
+                var a = int.Parse(Console.ReadLine());
+                array[i] = a;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"x[{i}] = {array[i]}");
+            }
+        }
+    }
+}
