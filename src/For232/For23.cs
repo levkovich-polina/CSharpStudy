862d6cffc7f78e076464dbd672b0d7a05f277aa8
Levkovich Polina <levkovich.polina@gmail.com>
2020-01-09 12:20:25 +0300

    #56

---
+++ src/For232/For23.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace For232
+{
+    class For23
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N");
+            var n = int.Parse(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("Число N должно быть больше 0");
+                return;
+            }
+
+            Console.WriteLine("Введите  число X");
+            var x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Sin={((-1) * x * x) / ((2 * n + 2) * (2 * n + 3))}");
+        }
+    }
+}
