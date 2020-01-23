e00d35adfc7e1acc4f38e9ca8d75e2f7c44bfb97
Levkovich Polina <levkovich.polina@gmail.com>
2020-01-23 18:28:08 +0300

    решила задачу Array03

---
+++ src/Array3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Array03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N ");
+            var n = int.Parse(Console.ReadLine());
+            if (n <= 1)
+            {
+                Console.WriteLine("Число N должно быть больше 1");
+                return;
+            }
+
+            Console.WriteLine("Введите целое число A ");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите целое число D ");
+            var d = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = array[i]+d;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"a[{i}] = {array[i]}");
+            }
+        }
+    }
+}
